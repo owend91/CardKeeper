35485c53c9fb735690cc1ac848b653035a227966
David Owen <owend91@gmail.com>
2023-01-09 09:01:41 -0700

    Initial Commit

---
+++ CardKeeper/CardKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardKeeperApp.swift
+//  CardKeeper
+//
+//  Created by David Owen on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CardKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
